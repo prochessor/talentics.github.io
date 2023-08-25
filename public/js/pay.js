1a26a0fe5982db61c4dacf201943bc92feca9393
Syed zaki haider <93634465+prochessor@users.noreply.github.com>
2023-08-25 04:19:23 -0700

    Delete public directory

--- public/js/pay.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#pay").addEventListener("click", async (e) => {
-    e.preventDefault();
-    let result = await fetch("http://localhost:8383/payment");
-    let data = await result.json();
-    console.log(data);
-    window.location.href = "http://localhost:8383/home.html"
-})
-
-
